af0937a71a52c36726ea6468bcacf909519265af
zbuthorne <zbuthorne@gmail.com>
2026-04-10 19:56:54 -0500

    Television python file

---
+++ television.py
@@ -0,0 +1,60 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__mute = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+
+
+    def power(self):
+        self.__status = not self.__status
+
+
+    def mute(self):
+        if self.__status:
+            self.__mute = not self.__mute
+
+
+    def channel_up(self):
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+
+
+    def channel_down(self):
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
+
+    def volume_up(self):
+        if self.__status:
+            if self.__status:
+                self.__mute = False
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+            else:
+                self.__volume = Television.MAX_VOLUME
+
+
+    def volume_down(self):
+        if self.__status:
+            if self.__status:
+                self.__mute = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+            else:
+                self.__volume = Television.MIN_VOLUME
+
+
+    def __str__(self):
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
